0a158e9526892f127a94af3058a355bdecc5c6a1
daydaywynn <dayday_wynn@alumni.baylor.edu>
2024-04-18 12:35:32 -0500

    Learning Exception Handling (Fourth Lesson) Learning more Exception Handling by removing the need for ExceptionController.java

--- src/main/java/guru/springframework/spring6restmvc/controller/ExceptionController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/ExceptionController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice //Makes this get picked up globally for all controller
+//@ControllerAdvice //Makes this get picked up globally for all controller
 public class ExceptionController {
 
     /**
